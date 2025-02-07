dc2d6e0e889e0a59d894e292fe9e48d8a247fc82
Ulises Rojas González <ulises.rogo@gmail.com>
2025-02-07 13:54:27 -0600

    2-17-Login-y-Logout

--- 01-react-foundation/src/components/LoginPage.tsx
+++ 01-react-foundation/src/components/LoginPage.tsx
@@ -1,13 +1,44 @@
+import { useEffect } from "react";
 import { useAuthStore } from "../store/auth.store"
 
 export const LoginPage = () => {
   const authStatus = useAuthStore( state => state.status );
+  const user = useAuthStore( state => state.user );
+
+  const login = useAuthStore( state => state.login );
+  const logout = useAuthStore( state => state.logout );
+
+  useEffect(() => {
+    setTimeout(() => {
+      logout();
+    }, 1500);
+  }, []);
+
+  if (authStatus === 'checking') {
+    return <h3>Loading...</h3>;
+  }
 
   return (
     <>
       <h3>Login Page</h3>
 
-      { authStatus }
+      {
+        ( authStatus === 'authenticated' )
+          ? <div>Autenticado como: { JSON.stringify(user, null, 2) }</div>
+          : <div>No Autenticado</div>
+      }
+
+      {
+        ( authStatus === 'authenticated')
+          ? (
+            <button onClick={ logout }>Logout</button>
+          )
+          : (
+            <button onClick={ () => login( 'fernando@google.com', '123' ) }>
+              Login
+            </button>
+          )
+      }
     </>
   )
 }
--- 01-react-foundation/src/store/auth.store.ts
+++ 01-react-foundation/src/store/auth.store.ts
@@ -7,11 +7,33 @@ interface AuthState {
         name: string;
         email: string;
     }
+
+    login: (email: string, password: string) => void;
+    logout: () => void;
 }
 
 export const useAuthStore = create<AuthState>()( (set) => ( {
     status: 'checking',
     token: undefined,
-    user: undefined
+    user: undefined,
+
+    login: (email: string, password: string) => {
+        set({
+            status: 'authenticated',
+            token: 'ABC123',
+            user: {
+                name: 'John Doe',
+                email: email,
+            }
+        })
+    },
+
+    logout: () => {
+        set({
+            status: 'unauthenticated',
+            token: undefined,
+            user: undefined,
+        })
+    },
 } )
 );

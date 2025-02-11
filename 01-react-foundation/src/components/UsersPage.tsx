1cbbc4a022b5fdb11a5dabddba52fab2d67fae97
Ulises Rojas González <ulises.rogo@gmail.com>
2025-02-10 20:04:48 -0600

    2-19-Establecer-el-tipo-Respuestas-HTTP

--- 01-react-foundation/src/components/UsersPage.tsx
+++ 01-react-foundation/src/components/UsersPage.tsx
@@ -1,10 +1,20 @@
 import axios from "axios";
 import { useEffect } from "react";
+import { ReqResUserListResponse } from "../interfaces";
+
+const loadUsers = async() => {
+  try {
+    const { data } = await axios.get<ReqResUserListResponse>('https://reqres.in/api/users');
+    return data.data;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
 
 export const UsersPage = () => {
   useEffect(() => {
-    axios.get('https://reqres.in/api/users?page=2')
-      .then( resp => console.log( resp.data ) );
+    loadUsers().then( users => console.log( users) );
 
     // fetch('https://reqres.in/api/users?page=2')
     //   .then( resp => resp.json() )

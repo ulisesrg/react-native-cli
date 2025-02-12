53429d3e8f2bce9dcdc2f569284113e861719095
Ulises Rojas González <ulises.rogo@gmail.com>
2025-02-12 00:53:47 -0600

    2-22-Custom-Hook-useUsers

---
+++ 01-react-foundation/src/components/UserRow.tsx
@@ -0,0 +1,17 @@
+import { User } from "../interfaces";
+
+interface Props {
+  user: User;
+}
+
+export const UserRow = ({ user }: Props) => {
+  const { avatar, first_name, last_name, email } = user;
+
+  return (
+    <tr>
+      <td> <img style={{ width: '50px' }} src={ avatar } alt="" /> </td>
+      <td>{ first_name } { last_name }</td>
+      <td>{ email }</td>
+    </tr>
+  );
+};
--- 01-react-foundation/src/components/UsersPage.tsx
+++ 01-react-foundation/src/components/UsersPage.tsx
@@ -1,54 +1,8 @@
-import axios from "axios";
-import { useEffect, useRef, useState } from "react";
-import type { ReqResUserListResponse, User } from "../interfaces";
-
-const loadUsers = async(page: number = 1): Promise<User[]> => {
-  try {
-    const { data } = await axios.get<ReqResUserListResponse>( 'https://reqres.in/api/users', {
-      params: {
-        page: page
-      }
-    });
-    return data.data;
-  } catch (error) {
-    console.log(error);
-    return [];
-  }
-}
+import { useUsers } from "../hooks/useUsers";
+import { UserRow } from "./UserRow";
 
 export const UsersPage = () => {
-  const [users, setUsers] = useState<User[]>([])
-  const currentPageRef = useRef(1);
-
-  useEffect(() => {
-    loadUsers(currentPageRef.current).then( setUsers ); // es lo mismo pero más reducido
-    // loadUsers().then( users => setUsers( users) );
-
-    // fetch('https://reqres.in/api/users?page=2')
-    //   .then( resp => resp.json() )
-    //   .then( data => console.log(data) );
-  }, []);
-
-  const nextPage = async() => {
-    currentPageRef.current++;
-    const users = await loadUsers(currentPageRef.current);
-
-    if ( users.length > 0 ) {
-      setUsers( users );
-    } else {
-      currentPageRef.current--;
-    }
-  }
-
-  const prevPage = async() => {
-    if ( currentPageRef.current < 1 ) {
-      return;
-    }
-
-    currentPageRef.current--;
-    const users = await loadUsers(currentPageRef.current);
-    setUsers( users );
-  }
+  const { users, nextPage, prevPage } = useUsers();
 
   return (
     <>
@@ -76,22 +30,5 @@ export const UsersPage = () => {
         <button onClick={ nextPage }>Next</button>
       </div>
     </>
-  )
-}
-
-
-interface Props {
-  user: User;
-}
-
-export const UserRow = ({ user }: Props) => {
-  const { avatar, first_name, last_name, email } = user;
-
-  return (
-    <tr>
-      <td> <img style={{ width: '50px' }} src={ avatar } alt="" /> </td>
-      <td>{ first_name } { last_name }</td>
-      <td>{ email }</td>
-    </tr>
   )
 }

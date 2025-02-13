8ffb077266843ada60acc83b53a0a471b2e2787e
Ulises Rojas González <ulises.rogo@gmail.com>
2025-02-12 19:06:42 -0600

    2-24-Envio-y-valores-del-formulario

--- 01-react-foundation/src/components/FormsPage.tsx
+++ 01-react-foundation/src/components/FormsPage.tsx
@@ -6,25 +6,35 @@ type FormInputs = {
 }
 
 export const FormsPage = () => {
-  const { register } = useForm<FormInputs>({
+  const { register, handleSubmit, formState, watch } = useForm<FormInputs>({
     defaultValues: {
       email: 'fernando@google.com',
       password: 'Abc123'
     }
   });
 
+  const onSubmit = ( myForm: FormInputs ) => {
+    console.log({ myForm });
+  }
+
+  console.log(watch( 'email' ));
+
   return (
     <>
-      <form>
+      <form onSubmit={ handleSubmit( onSubmit ) }>
         <h3>Formularios</h3>
 
         <div style={{ display: 'flex', flexDirection: 'column'}}>
-          <input type="text" placeholder="Email" { ...register('email') } />
+          <input type="text" placeholder="Email" { ...register('email', { required: true}) } />
           <input type="text" placeholder="Password" { ...register('password') } />
 
           <button type="submit">Ingresar</button>
         </div>
       </form>
+
+      <pre>
+        { JSON.stringify( formState, null, 2 ) }
+      </pre>
     </>
   )
 }

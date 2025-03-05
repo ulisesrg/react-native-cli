175a082a8185aeba6c8930cd2739426a1ef46350
Ulises Rojas González <ulises.rogo@gmail.com>
2025-03-05 11:44:05 -0600

    4-53-React-Native-Paper-Instalacion

--- 02-MyFirstApp/App.tsx
+++ 02-MyFirstApp/App.tsx
@@ -1,12 +1,15 @@
 import { SafeAreaView } from 'react-native';
 // import { HelloWorldScreen } from './src/presentation/screens/HelloWorldScreen';
 import { CounterScreen } from './src/presentation/screens/CounterScreen';
+import { PaperProvider } from 'react-native-paper';
 
 export const App = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      {/* <HelloWorldScreen name="Ulises Rojas González" /> */}
-      <CounterScreen />
-    </SafeAreaView>
+    <PaperProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        {/* <HelloWorldScreen name="Ulises Rojas González" /> */}
+        <CounterScreen />
+      </SafeAreaView>
+    </PaperProvider>
   );
 };
--- 02-MyFirstApp/src/presentation/screens/CounterScreen.tsx
+++ 02-MyFirstApp/src/presentation/screens/CounterScreen.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { PrimaryButton } from '../components';
+// import { PrimaryButton } from '../components';
+import { Button } from 'react-native-paper';
 
 export const CounterScreen = () => {
   const [count, setCount] = useState(10);
@@ -9,11 +10,13 @@ export const CounterScreen = () => {
     <View style={ styles.container }>
       <Text style={ styles.title }>{ count }</Text>
 
-      <PrimaryButton
-        label="Incrementar"
+      <Button
         onPress={ () => setCount( count + 1) }
         onLongPress={ () => setCount( 0 ) }
-      />
+        mode="contained"
+      >
+        Incrementar
+      </Button>
     </View>
   );
 };

59a84d30333fcd084b6506e15ae9cc70d391806f
Ulises Rojas González <ulises.rogo@gmail.com>
2025-03-12 18:46:05 -0600

    5-64-Padding-margin-border-width-y-height

--- 02-MyFirstApp/App.tsx
+++ 02-MyFirstApp/App.tsx
@@ -2,9 +2,10 @@ import { SafeAreaView } from 'react-native';
 // import { HelloWorldScreen } from './src/presentation/screens/HelloWorldScreen';
 // import { CounterScreen } from './src/presentation/screens/CounterScreen';
 import { PaperProvider } from 'react-native-paper';
-import { CounterM3Screen } from './src/presentation/screens/CounterM3Screen';
+// import { CounterM3Screen } from './src/presentation/screens/CounterM3Screen';
 
 import Icon from '@react-native-vector-icons/ionicons';
+import { BoxObjectModelScreen } from './src/presentation/screens/BoxObjectModelScreen';
 
 export const App = () => {
   return (
@@ -16,7 +17,8 @@ export const App = () => {
       <SafeAreaView style={{ flex: 1 }}>
         {/* <HelloWorldScreen name="Ulises Rojas González" /> */}
         {/* <CounterScreen /> */}
-        <CounterM3Screen />
+        {/* <CounterM3Screen /> */}
+        <BoxObjectModelScreen />
       </SafeAreaView>
     </PaperProvider>
   );
---
+++ 02-MyFirstApp/src/presentation/screens/BoxObjectModelScreen.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export const BoxObjectModelScreen = () => {
+  return (
+    <View style={ styles.container }>
+      <View style={ styles.purpleBox }>
+        <Text style={{ color: 'white' }}>Hola Mundo</Text>
+      </View>
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  title: {
+    fontSize: 30,
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+    borderWidth: 10,
+  },
+  purpleBox: {
+    height: 30,
+    backgroundColor: 'purple',
+    marginHorizontal: 20,
+    marginVertical: 50,
+    padding: 5,
+  },
+});

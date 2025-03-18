8b004117b00d25be1f0f3b485885d92d6a2b160d
Ulises Rojas González <ulises.rogo@gmail.com>
2025-03-17 20:24:58 -0600

    5-72-Align-Items

--- 02-MyFirstApp/src/presentation/screens/FlexDirectionScreen.tsx
+++ 02-MyFirstApp/src/presentation/screens/FlexDirectionScreen.tsx
@@ -15,22 +15,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#d1d1d1',
     justifyContent: 'space-between',
-    alignItems: 'center',
+    alignItems: 'stretch',
     flexDirection: 'row',
   },
   box: {
+    // flex: 1,
     width: 100,
     height: 100,
   },
   box1: {
     backgroundColor: '#5856D6',
-    top: -100,
+    // top: -100,
   },
   box2: {
     backgroundColor: '#4240a2',
   },
   box3: {
     backgroundColor: '#2e2d71',
-    top: 100,
+    // top: 100,
   },
 });

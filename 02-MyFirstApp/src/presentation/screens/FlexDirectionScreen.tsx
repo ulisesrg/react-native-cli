70efcbd295295d7d5d69d8e100d27ac162542fb5
Ulises Rojas González <ulises.rogo@gmail.com>
2025-03-17 20:38:07 -0600

    5-73-Align-Self

--- 02-MyFirstApp/src/presentation/screens/FlexDirectionScreen.tsx
+++ 02-MyFirstApp/src/presentation/screens/FlexDirectionScreen.tsx
@@ -6,6 +6,7 @@ export const FlexDirectionScreen = () => {
       <View style={[styles.box, styles.box1]} />
       <View style={[styles.box, styles.box2]} />
       <View style={[styles.box, styles.box3]} />
+      <View style={[styles.box, styles.box4]} />
     </View>
   );
 };
@@ -29,9 +30,12 @@ const styles = StyleSheet.create({
   },
   box2: {
     backgroundColor: '#4240a2',
+    alignSelf: 'flex-end',
   },
   box3: {
     backgroundColor: '#2e2d71',
-    // top: 100,
+  },
+  box4: {
+    backgroundColor: '#222155',
   },
 });

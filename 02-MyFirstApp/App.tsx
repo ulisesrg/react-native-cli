c6790aed6874a0336d4351d96129b990c66f1aad
Ulises Rojas González <ulises.rogo@gmail.com>
2025-03-17 20:12:33 -0600

    5-70-Flex

--- 02-MyFirstApp/App.tsx
+++ 02-MyFirstApp/App.tsx
@@ -6,7 +6,8 @@ import Icon from '@react-native-vector-icons/ionicons';
 // import { CounterM3Screen } from './src/presentation/screens/CounterM3Screen';
 // import { BoxObjectModelScreen } from './src/presentation/screens/BoxObjectModelScreen';
 // import { DimensionScreen } from './src/presentation/screens/DimensionScreen';
-import { PositionScreen } from './src/presentation/screens/PositionScreen';
+// import { PositionScreen } from './src/presentation/screens/PositionScreen';
+import { FlexScreen } from './src/presentation/screens/FlexScreen';
 
 
 export const App = () => {
@@ -23,7 +24,8 @@ export const App = () => {
         {/* <BoxObjectModelScreen /> */}
         {/* <BoxObjectModelScreen /> */}
         {/* <DimensionScreen /> */}
-        <PositionScreen />
+        {/* <PositionScreen /> */}
+        <FlexScreen />
       </SafeAreaView>
     </PaperProvider>
   );

ff929434fb20a77492e5926dd1c259a01abced96
Ulises Rojas González <ulises.rogo@gmail.com>
2025-03-17 20:48:19 -0600

    5-75-Preparacion-para-la-tarea

--- 02-MyFirstApp/App.tsx
+++ 02-MyFirstApp/App.tsx
@@ -8,7 +8,8 @@ import Icon from '@react-native-vector-icons/ionicons';
 // import { DimensionScreen } from './src/presentation/screens/DimensionScreen';
 // import { PositionScreen } from './src/presentation/screens/PositionScreen';
 // import { FlexScreen } from './src/presentation/screens/FlexScreen';
-import { FlexDirectionScreen } from './src/presentation/screens/FlexDirectionScreen';
+// import { FlexDirectionScreen } from './src/presentation/screens/FlexDirectionScreen';
+import { HomeworkScreen } from './src/presentation/screens/HomeworkScreen';
 
 
 export const App = () => {
@@ -27,7 +28,8 @@ export const App = () => {
         {/* <DimensionScreen /> */}
         {/* <PositionScreen /> */}
         {/* <FlexScreen /> */}
-        <FlexDirectionScreen />
+        {/* <FlexDirectionScreen /> */}
+        <HomeworkScreen />
       </SafeAreaView>
     </PaperProvider>
   );

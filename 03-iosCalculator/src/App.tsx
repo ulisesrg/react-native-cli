08712a2ed1b3a4e20a8556d2b8ecf3efb575161c
Ulises Rojas González <ulises.rogo@gmail.com>
2025-03-21 19:08:14 -0600

    6-84-Diseño-de-pantalla-y-estilos-globales

--- 03-iosCalculator/src/App.tsx
+++ 03-iosCalculator/src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import {StatusBar, View} from 'react-native';
-import { CalculatorScreen } from './presentation/screens/CalculatorScreen';
+import {CalculatorScreen} from './presentation/screens/CalculatorScreen';
+import {styles} from './config/theme/app-theme';
 
 function App() {
   return (
-    <View>
-      <StatusBar
-        barStyle={'light-content'}
-        backgroundColor={'black'}
-      />
+    <View style={styles.background}>
+      <StatusBar barStyle={'light-content'} backgroundColor={'black'} />
       <CalculatorScreen />
     </View>
   );
